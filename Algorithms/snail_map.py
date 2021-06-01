ae58f510ff4d6be52c08de093988c7302edd0cdb
24Chunks <alberto.mdza@yahoo.com>
2021-06-01 02:30:25 -0500

    Create snail_map.py

---
+++ Algorithms/snail_map.py
@@ -0,0 +1,50 @@
+lis = [
+    [1,2],
+    [3,4]
+]
+
+
+def func(snail_map):
+    output = []
+    # Scan right
+    x = 0
+    y = 0
+    dir = "RIGHT"
+    while len(snail_map) != 0:
+        if dir == "RIGHT" and snail_map:
+            while len(snail_map[0]) > 0:
+                output.append(snail_map[0][0])
+                del snail_map[0][0]
+            dir = "DOWN"
+            snail_map = [i for i in snail_map if i != []]
+        if dir == "DOWN" and snail_map:
+            y = 0
+            while y < len(snail_map):
+                output.append(snail_map[y][-1])
+                del snail_map[y][-1]
+                y += 1
+            dir = "LEFT"
+            snail_map = [i for i in snail_map if i != []]
+        if dir == "LEFT" and snail_map:
+            x = len(snail_map[-1]) - 1
+            while x >= 0:
+                output.append(snail_map[-1][x])
+                del snail_map[-1][x]
+                x-= 1
+            dir = "UP"
+            x = 0
+            snail_map = [i for i in snail_map if i != []]
+        if dir == "UP" and snail_map:
+            y = len(snail_map) - 1
+            while y >= 0:
+                output.append(l[y][x])
+                del snail_map[y][x]
+                y -= 1
+            dir = "RIGHT"
+            y = 0
+            snail_map = [i for i in snail_map if i != []]
+
+
+    return output
+
+print(func(lis))
